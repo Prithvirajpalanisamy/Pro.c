ae324c6aabf0e65c9952391c6cf021822c9d97f7
Prithvirajpalanisamy <35688112+Prithvirajpalanisamy@users.noreply.github.com>
2018-02-02 13:50:10 +0530

    Pro.C

---
+++ Pro.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+printf("Enter the number");
+scanf("%d",&a);
+if((a%2)=0)
+printf("The given number is even");
+else
+printf("The given number is odd");
+getch();
+return 0;
+}
